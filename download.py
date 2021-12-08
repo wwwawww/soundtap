f9bea53138fc5a0c8233c1d012ca14d47645d8df
wwwawww <93835318+wwwawww@users.noreply.github.com>
2021-12-08 18:37:30 +0200

    v3.0

--- download.py
+++ download.py
@@ -15,95 +15,63 @@ from pathlib import Path
 import re
 from urllib.parse import urlparse
 from colorama import Fore, Back, Style
-
-# Gets the current path
-path = str(Path(__file__).parent.absolute())
-
-# If config.json doesn't exist -> create it
-if not os.path.exists(path + "/config.json"):
-    f = open(path + "/config.json", 'a+')
-    # Default config.json
-    f.write('{"dlpath": "", "session": "", "format": "wav", "c2f": "y", "dac": "y", "dif": "y"}')
-    f.close()
-
-# Get configuration
-with open(path + "/config.json") as f:
-    config = json.load(f)
-    dlpath = path + "/" + (config["dlpath"])
-    # Session to log in
-    session = (config["session"])
-    # Format to lowercase, won't work otherwise
-    format = str.lower((config["format"]))
-    # Conversion to FLAC
-    c2f = (config["c2f"])
-    # Delete WAV after conversion
-    dac = (config["dac"])
-    # Delete invalid files
-    dif = (config["dif"])
-
-# If download path doesn't exist -> create it
-if not os.path.exists(dlpath):
-    os.makedirs(dlpath)
-    
-# Session
-s = requests.session()
-s.cookies.set("jb_SESSION", session, domain="soundtrap.com")
-# Disguising the downloader as a normal user
-s.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko"}
+from core import *
 
 # Download a project
 def downloadProject(id):
-    # Get ID from /studio/ URLs
+    id = str(id)
+    # Get ID from URLs
     if "soundtrap.com" in id:
         id = urlparse(id).path.rstrip('/').split('/')[-1]
-    # /stream/ to get WAV for free users.
     dlurl = "https://www.soundtrap.com/stream/" + id + "/s." + format
     apiurl = "https://www.soundtrap.com/api/project/getProject1/?id=" + id
-    caniaccessit = s.head(apiurl)
-    if caniaccessit.status_code == 404:
-        m = s.get(apiurl)
-        m = m.json()
-        ms = (m["error_message"])
-        print(Style.BRIGHT + Fore.RED + "(!) " + ms + Style.RESET_ALL)
-    elif caniaccessit.status_code == 409:
-        m = s.get(apiurl)
-        m = m.json()
-        mn = (m["name"])
-        ms = (m["message"])        
-        print(Style.BRIGHT + Fore.RED + "(!) " + mn + ": " + ms + Style.RESET_ALL)
+    # Get project data
+    r = s.head(apiurl)
+    if r.status_code == 404:
+        r = s.get(apiurl)
+        error = r.json()
+        message = error["error_message"]
+        print(Style.BRIGHT + Fore.RED + "(!) " + message + Style.RESET_ALL)
+    elif r.status_code == 409:
+        r = s.get(apiurl)
+        error = r.json()
+        title = error["name"]
+        message = error["message"]        
+        print(Style.BRIGHT + Fore.RED + "(!) " + title + ": " + message + Style.RESET_ALL)
     else:
-        rapi = s.get(apiurl)
-        pi = rapi.json()
-        title = (pi["title"])
-        ftitle = re.sub('\<|\>|\:|\"|\/|\\|\||\?|\*', '', title)
-        year = str((pi["lastUpdated"]))[:-3]
-        year = int(year)
-        year = datetime.fromtimestamp(year).isoformat()[:-9]
-        pio = (pi["owner"])
-        artist = (pio["fullName"])
-        pid = (pi["data"])
-        pid = json.loads(pid)
-        bpm = (pid["bpm"])
-        bpm = str(bpm)
-        key = (pid["key"])
+        r = s.get(apiurl)
+        project = r.json()
+        owner = project["owner"]
+        data = project["data"]
+        data = json.loads(data)
+        title = project["title"]
+        ctitle = re.sub('\<|\>|\:|\"|\/|\\|\||\?|\*', '', title)
+        artist = owner["fullName"]
+        date = str(project["lastUpdated"])[:-3]
+        date = int(date)
+        date = datetime.fromtimestamp(date).isoformat()[:-9]
+        bpm = str(data["bpm"])
+        key = data["key"]
+        # Download the project
         print(Style.BRIGHT + Fore.WHITE + "Downloading '" + Fore.GREEN + title + Fore.WHITE + "' [" + Fore.GREEN + id + Fore.WHITE + "] by '" + Fore.GREEN + artist + Fore.WHITE + "'..." + Style.RESET_ALL)
-        if os.path.isfile(dlpath + "/" + ftitle + "." + format):
+        if os.path.isfile(dlpath + "/" + ctitle + "." + format):
             print(Style.BRIGHT + Fore.RED + "(!) This project is already downloaded." + Style.RESET_ALL)
         else:
-            caniaccessit = s.head(dlurl)
-            caniaccessit.status_code
-            if caniaccessit.status_code == 404:
+            r = s.head(dlurl)
+            # If stream doesn't exist
+            if r.status_code == 404:
                 print(Style.BRIGHT + Fore.RED + "(!) This project can't be downloaded." + Style.RESET_ALL)
-            elif caniaccessit.status_code == 409:
-                m = s.get(dlurl)
-                m = m.json()
-                mn = (m["name"])
-                ms = (m["message"])        
-                print(Style.BRIGHT + Fore.RED + "(!) " + mn + ": " + ms + Style.RESET_ALL)
+            # If stream is unaccessible
+            elif r.status_code == 409:
+                r = s.get(dlurl)
+                error = r.json()
+                title = error["name"]
+                message = error["message"]        
+                print(Style.BRIGHT + Fore.RED + "(!) " + title + ": " + message + Style.RESET_ALL)
             else:
                 r = s.get(dlurl, stream=True)
                 total = int(r.headers.get('content-length', 0))
-                with open(dlpath + "/" + ftitle + "." + format, "wb") as file, tqdm(
+                with open(dlpath + "/" + ctitle + "." + format, "wb") as file, tqdm(
                     desc=Style.BRIGHT + Fore.WHITE + title,
                     total=total,
                     unit='iB',
@@ -115,9 +83,9 @@ def downloadProject(id):
                         bar.update(size)
                 print(Style.BRIGHT + Fore.WHITE + "Download finished." + Style.RESET_ALL)
                 # File Validation
-                with open(dlpath + "/" + ftitle + "." + format, encoding="cp850") as f:
+                with open(dlpath + "/" + ctitle + "." + format, encoding="cp850") as f:
                     deen = str(f.readlines())
-                    invalids = ["error", "message", "details"]
+                    invalids = ["error", "message", "details", "found"]
                     if re.compile('|'.join(invalids),re.IGNORECASE).search(deen):
                         print(Style.BRIGHT + Fore.WHITE + "[" + Fore.RED + "File Validator" + Fore.WHITE + "] Not a valid audio file." + Style.RESET_ALL)
                         invalidfile = True
@@ -126,68 +94,63 @@ def downloadProject(id):
                         invalidfile = False
                 if invalidfile == True:
                     if dif == "y":
-                        os.remove(dlpath + "/" + ftitle + "." + format)
+                        os.remove(dlpath + "/" + ctitle + "." + format)
                 elif invalidfile == False:
                     # MP3 Tags
                     if format == "mp3":
-                        audio = mutagen.File(dlpath + "/" + ftitle + ".mp3", easy=True)
+                        audio = mutagen.File(dlpath + "/" + ctitle + ".mp3", easy=True)
                         audio.add_tags()
                         audio["title"] = title
                         audio["artist"] = artist
-                        audio["date"] = year
-                        audio["copyright"] = artist
+                        audio["date"] = date
                         audio["bpm"] = bpm
-                        # I guess there is no key for this?
-                        #audio["key"] = key
-                        audio.save(dlpath + "/" + ftitle + ".mp3",v1=0)
+                        audio.save(dlpath + "/" + ctitle + ".mp3",v1=0)
                     # Automated conversion to FLAC
                     if format == "wav":
                         if c2f == "y":
-                            song = AudioSegment.from_wav(dlpath + "/" + ftitle + ".wav")
-                            if os.path.isfile(dlpath + "/" + ftitle + ".flac"):
+                            song = AudioSegment.from_wav(dlpath + "/" + ctitle + ".wav")
+                            if os.path.isfile(dlpath + "/" + ctitle + ".flac"):
                                 print(Style.BRIGHT + Fore.RED + "(!) This project is already converted." + Style.RESET_ALL)
                             else:
-                                song.export(dlpath + "/" + ftitle + ".flac", format="flac")
+                                song.export(dlpath + "/" + ctitle + ".flac", format="flac")
                                 # FLAC Tags
-                                audio = FLAC(dlpath + "/" + ftitle + ".flac")
+                                audio = FLAC(dlpath + "/" + ctitle + ".flac")
                                 audio["title"] = title
                                 audio["artist"] = artist
-                                audio["year"] = year
-                                audio["copyright"] = artist
+                                audio["year"] = date
                                 audio["bpm"] = bpm
                                 audio["key"] = key
                                 audio.save()
                                 # Remove WAV after conversion
                                 if dac == "y":
-                                    os.remove(dlpath + "/" + ftitle + ".wav")
+                                    os.remove(dlpath + "/" + ctitle + ".wav")
 
 # Download a folder
 def downloadFolder(id):
     global dlpath
-    pjscounter = int(0)
-    apiurl = "https://www.soundtrap.com/api/folder/getFolders1/?offset=0&count=999&order=ASC"
-    caniaccessit = s.head(apiurl)
-    if caniaccessit.status_code == 200:
-        try:
-            rapi = s.get(apiurl)
-            fi = rapi.json()
-            fip  = (fi[id])
-            fii = (fip["projectIds"])
-            nop = (fip["numberOfProjects"])
-            projectstitle = (fip["name"])
-            fipr = (fii[pjscounter])
-            fprojectstitle = re.sub('\<|\>|\:|\"|\/|\\|\||\?|\*', '', projectstitle)
-            dlpath = dlpath + "/" + fprojectstitle
+    try:
+        id = int(id)
+        id = id - 1
+        apiurl = "https://www.soundtrap.com/api/folder/getFolders1/?offset=0&count=999&order=ASC"
+        r = s.head(apiurl)
+        if r.status_code == 200:
+            r = s.get(apiurl)
+            folders = r.json()
+            folder  = folders[id]
+            projectIds = folder["projectIds"]
+            foldername = folder["name"]
+            cfoldername = re.sub('\<|\>|\:|\"|\/|\\|\||\?|\*', '', foldername)
+            dlpath = dlpath + "/" + cfoldername
             if not os.path.exists(dlpath):
                 os.makedirs(dlpath)
-            print(Style.BRIGHT + Fore.WHITE + "Downloading folder '" + Fore.GREEN + projectstitle + Fore.WHITE + "' [" + Fore.GREEN + str(id) + Fore.WHITE + "]..." + Style.RESET_ALL)
-            while pjscounter < nop:
-                fipr = (fii[pjscounter])
-                downloadProject(fipr)
-                pjscounter += int(1)
-            print(Style.BRIGHT + Fore.WHITE + "Downloading folder '" + Fore.GREEN + projectstitle + Fore.WHITE + "' finished." + Style.RESET_ALL)
+            print(Style.BRIGHT + Fore.WHITE + "Downloading folder '" + Fore.GREEN + foldername + Fore.WHITE + "' [" + Fore.GREEN + str(id + 1) + Fore.WHITE + "]..." + Style.RESET_ALL)
+            for projectId in projectIds:
+                downloadProject(projectId)
+            print(Style.BRIGHT + Fore.WHITE + "Downloading folder '" + Fore.GREEN + foldername + Fore.WHITE + "' finished." + Style.RESET_ALL)
             dlpath = path + "/" + (config["dlpath"])
-        except IndexError:
-            print(Style.BRIGHT + Fore.RED + "(!) This folder doesn't exist or is empty." + Style.RESET_ALL)
-    else:
-        print(Style.BRIGHT + Fore.RED + "(!) Folders couldn't be accessed." + Style.RESET_ALL)
+        else:
+            print(Style.BRIGHT + Fore.RED + "(!) API couldn't be accessed." + Style.RESET_ALL)
+    except ValueError:
+        print(Style.BRIGHT + Fore.RED + "(!) Folder ID has to be a integer.")
+    except IndexError:
+        print(Style.BRIGHT + Fore.RED + "(!) Folder doesn't exist.")
